ba3580f0ff5670233517b619efc09c7bff85d3ac
Yuki <yuuki.walsh@outlook.com>
2022-08-28 23:25:33 +1000

    added blur when images don't fit in the Image control

---
+++ Anamnesis/Controls/Image.xaml.cs
@@ -0,0 +1,28 @@
+﻿// © Anamnesis.
+// Licensed under the MIT license.
+
+namespace Anamnesis.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using XivToolsWpf.DependencyProperties;
+
+public partial class Image : UserControl
+{
+	public static readonly IBind<ImageSource?> SourceDp = Binder.Register<ImageSource?, Image>(nameof(Source), BindMode.OneWay);
+
+	public Image()
+	{
+		this.InitializeComponent();
+		this.ContentArea.DataContext = this;
+	}
+
+	public ImageSource? Source
+	{
+		get => SourceDp.Get(this);
+		set => SourceDp.Set(this, value);
+	}
+
+	public CornerRadius CornerRadius { get; set; }
+}
