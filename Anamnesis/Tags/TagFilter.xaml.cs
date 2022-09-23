dc2577fd9ab413360b2517f189971f3c54e9ce95
Yuki <yuuki.walsh@outlook.com>
2022-09-23 18:26:37 +1000

    further progress on tag filters

--- Anamnesis/Tags/TagFilter.xaml.cs
+++ Anamnesis/Tags/TagFilter.xaml.cs
@@ -4,14 +4,19 @@
 namespace Anamnesis.Tags;
 
 using PropertyChanged;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using XivToolsWpf.DependencyProperties;
 using XivToolsWpf.Extensions;
 
 [AddINotifyPropertyChangedInterface]
-public partial class TagFilter : UserControl
+public partial class TagFilter : UserControl, IComparer<Tag>
 {
-	public static readonly IBind<TagCollection?> AllTagsDp = Binder.Register<TagCollection?, TagFilter>(nameof(AllTags), OnAllTagsChanged, BindMode.OneWay);
+	public static readonly IBind<TagCollection> AllTagsDp = Binder.Register<TagCollection, TagFilter>(nameof(AllTags), BindMode.OneWay);
+
+	private readonly AddTag addTagItem = new();
 
 	public TagFilter()
 	{
@@ -19,28 +24,74 @@ public partial class TagFilter : UserControl
 		this.ContentArea.DataContext = this;
 	}
 
+	public FastObservableCollection<Tag> AvailableTags { get; init; } = new();
 	public FastObservableCollection<Tag> FilterByTags { get; init; } = new();
+	public bool IsPopupOpen { get; set; }
 
-	public TagCollection? AllTags
+	public TagCollection AllTags
 	{
 		get => AllTagsDp.Get(this);
 		set => AllTagsDp.Set(this, value);
 	}
 
-	private static void OnAllTagsChanged(TagFilter sender, TagCollection? value)
+	public int Compare(Tag? x, Tag? y) => string.Compare(x?.Name, y?.Name);
+
+	private void OnLoaded(object sender, RoutedEventArgs e)
 	{
-		if (value != null)
+		if (!this.FilterByTags.Contains(this.addTagItem))
 		{
-			sender.FilterByTags.Replace(value);
+			this.FilterByTags.Add(this.addTagItem);
 		}
-		else
+
+		this.AvailableTags.SortAndReplace(this.AllTags, this);
+	}
+
+	private void RemoveTag(Tag tag)
+	{
+		this.FilterByTags.Remove(tag);
+		this.AvailableTags.Add(tag);
+		this.AvailableTags.Sort(this);
+	}
+
+	private void AddTag(Tag tag)
+	{
+		this.FilterByTags.Insert(this.FilterByTags.Count - 1, tag);
+		this.AvailableTags.Remove(tag);
+		this.AvailableTags.Sort(this);
+	}
+
+	private void OnRemoveTagButtonClicked(object sender, RoutedEventArgs e)
+	{
+		if (sender is Button btn && btn.DataContext is Tag tag)
 		{
-			sender.FilterByTags.Clear();
+			this.RemoveTag(tag);
 		}
 	}
 
-	private void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+	private void OnAddTagButtonClicked(object sender, RoutedEventArgs e)
 	{
-		OnAllTagsChanged(this, this.AllTags);
+		if (sender is Button btn && btn.DataContext is Tag tag)
+		{
+			this.AddTag(tag);
+		}
+	}
+
+	private void OnTagSearchGotKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+	{
+		this.IsPopupOpen = true;
+		this.AddTagPopup.StaysOpen = true;
+	}
+
+	private void OnTagSearchLostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+	{
+		this.AddTagPopup.StaysOpen = false;
+	}
+}
+
+public class AddTag : Tag
+{
+	public AddTag()
+		: base("New Tag")
+	{
 	}
 }
