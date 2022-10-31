4d7746a72cac6c6789061ea48275a55a8ddd9725
Yuki <yuuki.walsh@outlook.com>
2022-10-31 17:49:53 +1100

    Fix flip sides checkbox in pose GUI not working

--- Anamnesis/Actor/Posing/Views/PoseGUIView.xaml.cs
+++ Anamnesis/Actor/Posing/Views/PoseGUIView.xaml.cs
@@ -3,15 +3,22 @@
 
 namespace Anamnesis.Actor.Views;
 
+using Anamnesis.Actor.Panels;
+using Anamnesis.Memory;
+using PropertyChanged;
 using System.Windows.Controls;
 
-/// <summary>
-/// Interaction logic for CharacterPoseView.xaml.
-/// </summary>
+[AddINotifyPropertyChangedInterface]
 public partial class PoseGuiView : UserControl
 {
 	public PoseGuiView()
 	{
 		this.InitializeComponent();
+		this.ContentArea.DataContext = this;
 	}
+
+	// TODO: this might be better as a setting, or a value per actor
+	public bool FlipSides { get; set; }
+
+	public ActorMemory? Actor => (this.DataContext as ActorPanelBase)?.Actor;
 }
